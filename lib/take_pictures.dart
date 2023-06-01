cd2a726f7930adfce301ec06f4eb345b7b895dc6
Pimegonho <pimegonho@protonmail.com>
2023-05-31 21:54:51 -0300

    tela de tirar foto

--- lib/take_pictures.dart
+++ lib/take_pictures.dart
@@ -6,12 +6,87 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () async {
-        await FirebaseAuth.instance.signOut();
-        if (context.mounted) Navigator.pop(context);
-      },
-      child: const Text('d'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/take_pictures_pic.png',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await FirebaseAuth.instance.signOut();
+                        if (context.mounted) Navigator.pop(context);
+                      },
+                      child: const Icon(Icons.arrow_back),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 200,
+                ),
+                Column(children: [
+                  const Text(
+                    'Tire 3 fotos da boca fudida da criança',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                          size: 80,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                          size: 80,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                          size: 80,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ]),
+              ],
+            ),
+          ),
+          SafeArea(
+            child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Text('Continuar'))),
+          )
+        ],
+      ),
     );
   }
 }
