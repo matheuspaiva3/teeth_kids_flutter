afcf7903be1f5cc6d608b0fcbf204e1e61406356
Pimegonho <pimegonho@protonmail.com>
2023-06-02 16:10:34 -0300

    merda de camera funcionando e foto pro storage

--- lib/take_pictures.dart
+++ lib/take_pictures.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'camera.dart' as camera;
-
+import 'dart:async';
+import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
-
 import 'package:camera/camera.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart';
+import 'package:path/path.dart' as path;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -64,21 +63,54 @@ class HomePage extends StatelessWidget {
                           Icons.add_a_photo,
                           size: 80,
                         ),
-                        onPressed: () {},
+                        onPressed: () async {
+                          final cameras = await availableCameras();
+                          final firstCamera = cameras.first;
+                          if (context.mounted) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      TakePictureScreen(camera: firstCamera)),
+                            );
+                          }
+                        },
                       ),
                       IconButton(
                         icon: const Icon(
                           Icons.add_a_photo,
                           size: 80,
                         ),
-                        onPressed: () {},
+                        onPressed: () async {
+                          final cameras = await availableCameras();
+                          final firstCamera = cameras.first;
+                          if (context.mounted) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      TakePictureScreen(camera: firstCamera)),
+                            );
+                          }
+                        },
                       ),
                       IconButton(
                         icon: const Icon(
                           Icons.add_a_photo,
                           size: 80,
                         ),
-                        onPressed: () {},
+                        onPressed: () async {
+                          final cameras = await availableCameras();
+                          final firstCamera = cameras.first;
+                          if (context.mounted) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      TakePictureScreen(camera: firstCamera)),
+                            );
+                          }
+                        },
                       ),
                     ],
                   ),
@@ -95,5 +127,144 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
+
+  final CameraDescription camera;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tire a foto')),
+      // You must wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner until the
+      // controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Attempt to take a picture and get the file `image`
+            // where it was saved.
+            final image = await _controller.takePicture();
+
+            if (!mounted) return;
+
+            // If the picture was taken, display it on a new screen.
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  // Pass the automatically generated path to
+                  // the DisplayPictureScreen widget.
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final FirebaseStorage storage = FirebaseStorage.instance;
+  final String imagePath;
+  final String? userId = FirebaseAuth.instance.currentUser?.uid;
+
+  DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Foto pronta')),
+      body: Column(
+        children: [
+          Image.file(File(imagePath)),
+          ElevatedButton(
+            onPressed: () {
+              uploadImageToFirebase();
+            },
+            child: const Text('Confirmar'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> uploadImageToFirebase() async {
+    File imageFile = File(imagePath);
+    String fileName = path.basename(imageFile.path);
+    try {
+      TaskSnapshot snapshot = await storage
+          .ref()
+          .child('users/$userId/$fileName')
+          .putFile(imageFile);
+      String downloadUrl = await snapshot.ref.getDownloadURL();
+      // Do something with the download URL if needed
+      print('Image uploaded to Firebase Storage: $downloadUrl');
+    } catch (e) {
+      // Handle the error
+      print(e.toString());
+    }
   }
 }
